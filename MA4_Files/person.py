2a358d75aa8002231507e5f04251a95303001c7b
yoonsung713 <sandyj0713@naver.com>
2023-10-11 16:58:09 +0200

    sdfg

--- MA4_Files/person.py
+++ MA4_Files/person.py
@@ -10,7 +10,7 @@ class Person(object):
 		lib.Person_get.restype = ctypes.c_int
 		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Person_delete.argtypes = [ctypes.c_void_p]
-		lib.Person_fib.argtypes = [ctypes.c_int]
+		lib.Person_fib.argtypes = [ctypes.c_void_p]
 		lib.Person_fib.restypes = ctypes.c_int
 		self.obj = lib.Person_new(age)
 
