167d6ef8310596972f5dbe6f238efd9594653b60
yoonsung713 <sandyj0713@naver.com>
2023-10-11 17:00:09 +0200

    sdfghj

--- MA4_Files/person.cpp
+++ MA4_Files/person.cpp
@@ -43,7 +43,7 @@ extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
-	int Person_fib(Person* person, int n) {return person->fib(n);}
+	int Person_fib(Person* person) {return person->fib();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
