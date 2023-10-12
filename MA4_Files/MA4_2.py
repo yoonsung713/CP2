cf3b215e6d38bcf52388c29fd3476acc83191bc6
yoonsung713 <sandyj0713@naver.com>
2023-10-13 01:33:31 +0200

    sdfg

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -2,8 +2,16 @@
 
 from person import Person
 from numba import njit
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
 
 @njit
+def fib_numba(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_numba(n-1) + fib_numba(n-2))
+	
 def fib_py(n):
 	if n <= 1:
 		return n
@@ -12,10 +20,15 @@ def fib_py(n):
 
 def main():
 	f = Person(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+	start = pc()
+	# print(f.get())
+	# f.set(7)
+	# print(f.get())
+	print(fib_numba(10))
+	print(fib_py(10))
 	print(f.fib())
+	end = pc()
+
 
 if __name__ == '__main__':
 	main()
