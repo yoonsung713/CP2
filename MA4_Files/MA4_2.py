d991beed883b70f9dec006a49144fde97911e673
yoonsung713 <sandyj0713@naver.com>
2023-10-13 01:42:22 +0200

    sdfg

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -24,13 +24,44 @@ def main():
 	# print(f.get())
 	# f.set(7)
 	# print(f.get())
-	print(fib_numba(10))
-	end = pc()
-	print(fib_py(10))
-	end2 = pc()
-	print(start - end)
-	print(end - end2)
-	print(f.fib())
+	# print(fib_numba(10))
+	# end = pc()
+	# print(fib_py(10))
+	# end2 = pc()
+	# print(start - end)
+	# print(end - end2)
+	# print(f.fib())
+	ns = list(range(30, 46))  # Values of n from 30 to 45
+	py_times = []
+	numba_times = []
+	cpp_times = []
+
+	for n in ns:
+		start = pc()
+		fib_py(n)
+		end = pc()
+		py_times.append(end - start)
+		
+		start = pc()
+		fib_numba(n)
+		end = pc()
+		numba_times.append(end - start)
+		
+		f = Person(n)
+		start = pc()
+		f.fib()
+		end = pc()
+		cpp_times.append(end - start)
+
+	plt.figure()
+	plt.plot(ns, py_times, label="Python")
+	plt.plot(ns, numba_times, label="Numba")
+	plt.plot(ns, cpp_times, label="C++")
+	plt.xlabel("n")
+	plt.ylabel("Time (seconds)")
+	plt.legend()
+	#plt.savefig("fibonacci_performance.png")
+	plt.close()
 
 
 if __name__ == '__main__':
